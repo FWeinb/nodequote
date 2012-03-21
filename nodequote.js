2d8b5647a73c6f3e007e1270bbb8338efbbb4960
Fabrice Weinberg <Fabrice@weinberg.me>
2012-03-21 20:38:32 +0100

    Fix thousands of bugs. Add a test for every function to test.js

--- nodequote.js
+++ nodequote.js
@@ -7,19 +7,31 @@ var request = require('request');
 
 var nodeQuote = (function(){
 
+	var BASE  = 'https://quote.fm/api/';
+
+
 	/* Helper Functions */
 		function addPage(page, start){
-			var q = ((start === null)?"&":start)+"page="
-			if (Array.isArray(page)){
-				return q+page.join(",");			
-			}else if (isNumeric(page) && page != 0){
-				q += page;
-				return q;
+			if (page){
+				var q = ((start === null)?"&":start)+"page="
+				if (Array.isArray(page)){
+					return q+page.join(",");			
+				}else if (isNumeric(page) && page != 0){
+					q += page;
+					return q;
+				}
 			}
 			return "";
 		}
 
-		function convertIdsToString(ids){
+		function serializeObject(object){
+			var query = "";
+			for(var name in object){
+				query += name + "=" + covertArrayToString(object[name]);
+			}
+			return query;
+		}
+		function covertArrayToString(ids){
 			if (Array.isArray(ids)){
 				return ids.join(",");
 			}
@@ -33,15 +45,15 @@ var nodeQuote = (function(){
 
 
 	/* Private Functions */
-
-		function getRecommendationList(id, page, type, callback){
-			request(this.BASE + "recommendation/"+type+"/?id=" + id + this.addPage(page), function(e, r, b){
+		function getRecommendationList(para, page, type, callback){
+			var query = ((isNumeric(para))?"?id=":"?username=") + para;
+			request(BASE + "recommendation/"+type+"/"+ query + addPage(page), function(e, r, b){
 				processCallback(e, r, b, callback);
 			});			
 		}
 
-		var getUserList  =  function(username, page, type, callback){
-			request(this.BASE + "user/"+type+"/?username=" + username + this.addPage(page), function(e, r, b){
+		function getUserList(username, page, type, callback){
+			request(BASE + "user/"+type+"/?username=" + username + addPage(page), function(e, r, b){
 				processCallback(e, r, b, callback);
 			});	
 		}
@@ -58,8 +70,6 @@ var nodeQuote = (function(){
 
 		API_VERSION : '1.0.3',
 
-		BASE : 'https://quote.fm/api/',
-
 		/* Recommandation */
 
 		getRecommendation : function (id, callback){
@@ -67,15 +77,17 @@ var nodeQuote = (function(){
 		},
 
 		getRecommendationListByArticle : function (id, page, callback){
+			callback = (arguments.length === 2)?page:callback;
 			getRecommendationList(id, page, "listByArticle", callback);
 		},
 
-		getRecommendationListByUser : function (id, page, callback){
-			getRecommendationList(id, page, "listByUser", callback);
+		getRecommendationListByUser : function (name, page, callback){
+			callback = (arguments.length === 2)?page:callback;
+			getRecommendationList(name, page, "listByUser", callback);
 		},
 
 		/* Article */
-		
+
 		getArticle : function(para, callback){
 			if (isNumeric(para)){
 				this.getArticleById(para, callback);
@@ -85,26 +97,26 @@ var nodeQuote = (function(){
 		},
 
 		getArticleByUrl : function(url, callback){
-			request(this.BASE + "article/get/?url=" + url, function(e, r, b){
+			request(BASE + "article/get/?url=" + url, function(e, r, b){
 				processCallback(e, r, b, callback);
 			});
 		},
 
 		getArticleById : function(id, callback){
-			request(this.BASE + "article/get/?id=" + id, function(e, r, b){
+			request(BASE + "article/get/?id=" + id, function(e, r, b){
 				processCallback(e, r, b, callback);
 			});
 		},
 
-
-		getArticleListByPage : function(id, page, callback){
-			request(this.BASE + "article/listByPage/?id=" + id + this.addPage(page), function(e, r, b){
+		getArticleListByPage : function(id, page, callback){			
+			callback = (arguments.length === 2)?page:callback;
+			request(BASE + "article/listByPage/?id=" + id + addPage(page), function(e, r, b){
 				processCallback(e, r, b, callback);
 			});
 		},
 
-		getArticleListByCategories : function(ids, page, language, scope, callback){
-			request(this.BASE + "article/listByCategories/?ids=" + convertIdsToString(ids) + this.addPage(page) + "&language="+language + "&scope="+scope, function(e, r, b){
+		getArticleListByCategories : function(object, callback){
+			request(BASE + "article/listByCategories/?" + serializeObject(object) , function(e, r, b){
 				processCallback(e, r, b, callback);
 			});
 		},
@@ -115,24 +127,25 @@ var nodeQuote = (function(){
 			if (isNumeric(para)){
 				this.getPageById(para, callback);
 			}else{
-				this.getPageByUrl(para, callback);
+				this.getPageByDomain(para, callback);
 			}
 		},
 
 		getPageById : function(id, callback){
-			request(this.BASE + "page/get/?id=" + id, function(e, r, b){
+			request(BASE + "page/get/?id=" + id, function(e, r, b){
 				processCallback(e, r, b, callback);
 			});
 		},
 
 		getPageByDomain : function(domain, callback){
-			request(this.BASE + "page/get/?domain=" + domain, function(e, r, b){
+			request(BASE + "page/get/?domain=" + domain, function(e, r, b){
 				processCallback(e, r, b, callback);
 			});
 		},
 
-		getPageList : function(page, callback){
-			request(this.BASE + "page/list/" + this.addPage(page, "?"), function(e, r, b){
+		getPageList : function(page, callback){		
+			callback = (arguments.length === 1)?page:callback;
+			request(BASE + "page/list/" + addPage(page, "?"), function(e, r, b){
 				processCallback(e, r, b, callback);
 			});
 		},
@@ -148,29 +161,31 @@ var nodeQuote = (function(){
 		},
 
 		getUserByName : function(name, callback){
-			request(this.BASE + "user/get/?username=" + name, function(e, r, b){
+			request(BASE + "user/get/?username=" + name, function(e, r, b){
 				processCallback(e, r, b, callback);
 			});
 		},
 
 		getUserById : function(id, callback){
-			request(this.BASE + "user/get/?id=" + id, function(e, r, b){
+			request(BASE + "user/get/?id=" + id, function(e, r, b){
 				processCallback(e, r, b, callback);
 			});
 		},
 
 		getUserListFollowers : function(username, page, callback){
+			callback = (arguments.length === 2)?page:callback;
 			getUserList(username, page, "listFollowers", callback);
 		},
 
 		getUserListFollowings : function(username, page, callback){
+			callback = (arguments.length === 2)?page:callback;
 			getUserList(username, page, "listFollowings", callback);
 		},
 
 
 		/* Category */
-		getCategoryList : function(){
-			request(this.BASE + "category/list", function(e, r, b){
+		getCategoryList : function(callback){
+			request(BASE + "category/list", function(e, r, b){
 				processCallback(e, r, b, callback);
 			});
 		},
--- test.js
+++ test.js
@@ -1,7 +1,98 @@
 var nodeQuote = require("./nodequote.js");
 
-nodeQuote.getUser("uarrr", function(j, e){
-	if (!e){
-		console.log(j);
-	}
-});
+/* Recommandation */
+	nodeQuote.getRecommendation(900, function(j, e){
+		if (!e){
+			console.log("getRecomendation:\t" + j.user.fullname);
+		}
+	});
+
+	nodeQuote.getRecommendationListByArticle(123, 0, function(j, e){ // You can omit the 0 for page
+		if (!e){
+			console.log("getRecommendationListByArticle:\t" + j.article.url);
+		}
+	});
+
+	nodeQuote.getRecommendationListByUser("uarrr", 0, function(j, e){ // You can omit the 0 for page
+		if (!e){
+			console.log("getRecommendationListByUser:\t" + j.user.fullname);
+		}
+	});
+
+
+/* Article */
+	nodeQuote.getArticle(2111, function(j, e){
+		if (!e){
+			console.log("getArticle(id):\t" + j.topquote.user_id);
+		}
+	});
+
+	nodeQuote.getArticle("http://www.spiegel.de/kultur/literatur/0,1518,790592-2,00.html", function(j, e){
+		if (!e){
+			console.log("getArticle(url):\t" + j.topquote.user_id);
+		}
+	});
+
+	nodeQuote.getArticleListByPage(23, 0, function(j, e){ // You can omit the 0 for page
+		if (!e){
+			console.log("getArticleListByPage:\t" + j.page.name);
+		}
+	});
+
+	nodeQuote.getArticleListByCategories({ids : [1,2,3], language : "de", page : [5,6]}, function(j, e){ // You can omit the 0 for page
+		if (!e){
+			console.log("getArticleListByCategories:\t" + j.totalCount);
+		}
+	});
+
+/* Page */
+	nodeQuote.getPage(24,  function(j, e){ 
+		if (!e){
+			console.log("getPage(id):\t" + j.name);
+		}
+	});
+
+	nodeQuote.getPage("zeit.de",  function(j, e){ 
+		if (!e){
+			console.log("getPage(domain):\t" + j.id);
+		}
+	});
+
+	nodeQuote.getPageList(0, function(j, e){ // You can omit the 0 for page
+		if (!e){
+			console.log("getPageList:\t" + j.totalCount);
+		}
+	});
+
+/* User */
+	nodeQuote.getUser(1229,  function(j, e){ 
+		if (!e){
+			console.log("getUser(id):\t" + j.username);
+		}
+	});
+
+	nodeQuote.getUser("FWeinb",  function(j, e){ 
+		if (!e){
+			console.log("getUser(name):\t" + j.id);
+		}
+	});
+
+	nodeQuote.getUserListFollowers("FWeinb", 0,  function(j, e){ // You can omit the 0 for page
+		if (!e){
+			console.log("getUserListFollowers:\t" + j.totalCount);
+		}
+	});
+
+	nodeQuote.getUserListFollowings("FWeinb", function(j, e){ // You can omit the 0 for page
+		if (!e){
+			console.log("getUserListFollowings:\t" + j.totalCount);
+		}
+	});
+
+/* Category */
+	nodeQuote.getCategoryList(function(j, e){ 
+		if (!e){
+			console.log("getCategoryList:\t" + j.totalCount);
+		}
+	});
+
